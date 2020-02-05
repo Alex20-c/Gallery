ec8a840e05a7e291370104000c9ea0a5f4e9f9bb
Alex Kiprop <kipropalex59@gmail.com>
2020-02-05 09:19:39 +0300

    test class to test the behaviour of the editor

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    def setUp(self):
+        self.killa=  Editor(first_name = 'machel', last_name ='killa', email ='killa@gmail.com')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.killa,Editor))
+        
+    def test_save_method(self):
+        self.killa.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+        
+class tagsTestClass(TestCase):
+    def setUp(self):
+        self.world= tags(name='world')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.world,tags))
+        
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.London= Location(name='London')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.London,Location))
+        
+    def test_save_method(self):
+        self.London.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+        
+        
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.culture= Category(name='cool')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.culture,Category))
+        
+    def test_save_method(self):
+        self.culture.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+        
